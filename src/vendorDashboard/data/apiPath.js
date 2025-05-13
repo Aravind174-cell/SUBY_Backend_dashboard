0a679789e7da9c05b2f064aa0c6f20bd5db4946b
Aravind175-cell <avulaaravind00789@gmail.com>
2025-05-13 22:45:49 +0530

    5 th commit

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,4 +1,4 @@
 
-export const API_URL = "http://localhost:4000"
+// export const API_URL = "http://localhost:4000"
 
-// export const API_URL = "https://backend-node-js-suby-1-acrz.onrender.com";
+export const API_URL = "https://backend-node-js-suby-1-acrz.onrender.com";
